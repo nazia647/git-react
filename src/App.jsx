25493025fa8ca06bfcb982f80beb40a9407b3271
Nazia <naziaasgharkpo24@gmail.com>
2024-08-12 16:22:57 +0500

    folder

--- src/App.jsx
+++ src/App.jsx
@@ -37,16 +37,60 @@
 
 
 
-import React from 'react'
+// import React from 'react'
 
-function App() {                 
+// function App() {                 
+//   return (
+//     <div>
+//       <h1>hello</h1>
+//       <p> i m nazia</p>
+//       <P>it section</P>
+//     </div>
+//   )
+// }
+
+// export default App
+   
+
+
+
+
+
+// topic Hook starts
+
+import React, { useEffect, useMemo, useState } from 'react';
+
+function Hook() {
+    const [count ,setCount]  = useState(0);
+    const [data ,setData]  = useState(10);
+//  useEffect(()=>{
+//     const array = new Array(1000000000).fill(0)
+//     console.log(array)
+//     // console.log("soory working")
+//  },[count]) 
+//  useEffect(()=>{
+//     console.log("data increase") 
+//  },[data]) 
+ const control = useMemo(()=>{
+    return(
+        <div>
+            <h1 style={{color:"red"}} >Count in memo: {count} data {data} </h1>
+        </div>
+    )
+ },[count])
   return (
     <div>
-      <h1>hello</h1>
-      <p> i m nazia</p>
-      <P>it section</P>
+       
+
+    {control}
+
+  <h1>Count: {count}</h1>
+  <h1>Data: {data}</h1>
+
+  <button onClick={()=>{setCount(count+1)}} >increment count</button>
+  <button onClick={()=>{setData(data+10)}} >increment data</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default Hook;
